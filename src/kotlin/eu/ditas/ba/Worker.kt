58e43c5391df506afad774b6ff7a6446f5a80ba1
durgen <richard.girke@campus.tu-berlin.de>
2019-08-21 15:52:18 +0200

    added refresh token parsing

--- src/kotlin/eu/ditas/ba/Worker.kt
+++ src/kotlin/eu/ditas/ba/Worker.kt
@@ -2,9 +2,13 @@ package eu.ditas.ba
 
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.coroutines.awaitByteArrayResponseResult
 import com.github.kittinunf.fuel.coroutines.awaitStringResult
+import com.github.kittinunf.fuel.gson.gsonDeserializer
+import com.github.kittinunf.fuel.gson.gsonDeserializerOf
+import com.github.kittinunf.fuel.httpPost
 import kotlinx.coroutines.*
 import mu.KotlinLogging
 import java.lang.System.currentTimeMillis
@@ -12,10 +16,17 @@ import java.util.concurrent.ConcurrentLinkedQueue
 import com.github.kittinunf.result.Result
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import io.jsonwebtoken.Jwts
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
+import java.util.ArrayList
+import io.jsonwebtoken.Jwt
+
+
+
 
 private val logger = KotlinLogging.logger {}
 
@@ -24,19 +35,19 @@ class Worker(private val payload: Payload) {
     private val workers: MutableList<WorkerThread> = mutableListOf()
     private var experimentStart: Long = 0;
 
-    class WorkerThread(val id:Int,private val requests: List<Requests>) {
+    class WorkerThread(val id: Int, private val requests: List<Requests>) {
 
         val results: ConcurrentLinkedQueue<WorkloadResult> = ConcurrentLinkedQueue()
 
         suspend fun warmup(warmup: Int) {
-                withTimeoutOrNull(warmup.toLong()) {
-                    requests.forEach {
-                        val (body, fuelError) = Fuel.request(it.method, it.path)
-                                .header(it.requestHeader)
-                                .useHttpCache(false)
-                                .awaitStringResult()
-                    }
+            withTimeoutOrNull(warmup.toLong()) {
+                requests.forEach {
+                    val (body, fuelError) = Fuel.request(it.method, it.path)
+                            .header(it.requestHeader)
+                            .useHttpCache(false)
+                            .awaitStringResult()
                 }
+            }
 
 
         }
@@ -50,12 +61,12 @@ class Worker(private val payload: Payload) {
                         .useHttpCache(false)
 
                 val (_, response, result) = request.awaitByteArrayResponseResult()
-                processReply(it,itter, launchTime, response, result)
+                processReply(it, itter, launchTime, response, result)
 
             }
         }
 
-        private  fun processReply(request:Requests,itter: Int, launchTime:Long, response:Response,result:Result<ByteArray,FuelError>){
+        private fun processReply(request: Requests, itter: Int, launchTime: Long, response: Response, result: Result<ByteArray, FuelError>) {
             val latancy = currentTimeMillis() - launchTime
             val statusCode = response.statusCode
             val (data, error) = result
@@ -65,7 +76,7 @@ class Worker(private val payload: Payload) {
                 body = String(data)
             }
 
-            results.add(WorkloadResult(request.id,id,itter,latancy,statusCode,body,response.headers,error != null))
+            results.add(WorkloadResult(request.id, id, itter, latancy, statusCode, body, response.headers, error != null))
         }
 
         override fun toString(): String {
@@ -74,7 +85,36 @@ class Worker(private val payload: Payload) {
     }
 
     private fun getValidAccessToken(): String {
-        return payload.token
+
+        // delete signature since we don't need it but the library will not parse w/o key when token is singed
+        val i = payload.token.lastIndexOf('.')
+        val withoutSignature = payload.token.substring(0, i+1);
+
+        //parse token
+        val jwtBody= Jwts.parser().parseClaimsJwt(withoutSignature).body
+        val realmURL = jwtBody.get("iss").toString() + "/protocol/openid-connect"
+        val tokenURL="$realmURL/token"
+        val clientId = jwtBody.get("azp").toString()
+
+        // get the access token on the token endpoint
+        val (request, response, result) = tokenURL.httpPost(listOf(Pair("client_id", clientId), Pair("grant_type", "refresh_token"), Pair("refresh_token", payload.token)))
+                .responseObject(gsonDeserializerOf(JsonObject::class.java))
+        val (body, error) = result
+        var ret = "";
+
+        // check if access token is present
+        if (error != null) {
+            logger.error { "unable to get access token due to ${error.message}" }
+        } else {
+            if (body == null) {
+                logger.error { "unable to get access token due to empty response body" }
+            } else {
+                ret = body.get("access_token").asString
+            }
+        }
+        logger.info { "building request using access_token: [$ret]" }
+        return ret;
+
     }
 
     fun prepare() {
@@ -90,14 +130,14 @@ class Worker(private val payload: Payload) {
 
         }
         //XXX: do this based on the strategy
-        for (thread in 1.rangeTo(payload.threads)){
+        for (thread in 1.rangeTo(payload.threads)) {
             val requests = mutableListOf<Requests>()
             //adding all request sequentially
             for (reqs in payload.requests) {
                 requests.addAll(reqs)
             }
 
-            workers.add(WorkerThread(thread,requests))
+            workers.add(WorkerThread(thread, requests))
         }
 
         //setup measurements
@@ -114,7 +154,7 @@ class Worker(private val payload: Payload) {
         assert(payload.strategy == "sequential")
         val results = mutableListOf<Deferred<Unit>>()
         runBlocking {
-            for(worker in workers){
+            for (worker in workers) {
                 results += async {
                     worker.warmup(payload.warmup)
                     logger.info { "[${worker.id}] finished warm up" }
@@ -133,9 +173,8 @@ class Worker(private val payload: Payload) {
     }
 
 
-
     @ExperimentalStdlibApi
-    fun cleanup() : BenchmarkResults{
+    fun cleanup(): BenchmarkResults {
         val totalExperimentTime = currentTimeMillis() - experimentStart
         logger.info { "totalTime : $totalExperimentTime" }
 
@@ -153,8 +192,8 @@ class Worker(private val payload: Payload) {
         try {
             Files.write(Paths.get("/tmp/${results.runnerID}.json"), Gson().toJson(results).encodeToByteArray());
             logger.info { "wrtitten results to /tmp/${results.runnerID}.json" }
-        } catch (e:IOException) {
-            logger.error("failed to write backup",e)
+        } catch (e: IOException) {
+            logger.error("failed to write backup", e)
         }
 
         return results
